05a4bfb3143cec662baf0bb46fed6f8df954754a
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-08 10:48:54 +0000

    updating urls.py to reflect the template folder

--- Budget_Manager/urls.py
+++ Budget_Manager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='home'),  # Set dashboard as the home page
 ]
