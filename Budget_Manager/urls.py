eca0f33a12d2f9429eb22bb586debeafe00b7927
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-09 13:56:53 +0000

    updating urls.py with sign up url

--- Budget_Manager/urls.py
+++ Budget_Manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add_income/', views.add_income, name='add_income'),
     path('add_expense/', views.add_expense, name='add_expense'),
