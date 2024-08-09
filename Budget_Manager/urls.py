afe21b96b55781b286ce88055683b05a5fea085f
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-09 14:12:18 +0000

    updating views.py and urls.py with sign up button on login page

--- Budget_Manager/urls.py
+++ Budget_Manager/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add_income/', views.add_income, name='add_income'),
     path('add_expense/', views.add_expense, name='add_expense'),
     path('delete_income/<int:income_id>/', views.delete_income, name='delete_income'),
     path('delete_expense/<int:expense_id>/', views.delete_expense, name='delete_expense'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),  # Sign-Up URL
     path('', views.dashboard, name='home'),
 ]
--- Budget_Manager/views.py
+++ Budget_Manager/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 from .models import Income, Expense
 from .forms import IncomeForm, ExpenseForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('dashboard')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required
 def dashboard(request):
@@ -104,3 +94,8 @@ def delete_expense(request, expense_id):
         return JsonResponse({'success': True})
     except Expense.DoesNotExist:
         return JsonResponse({'success': False, 'error': 'Expense not found.'})
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
