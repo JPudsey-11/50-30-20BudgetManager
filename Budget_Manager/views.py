bb1032c37362f61573ebfaed89f0ef54e22384b5
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-09 13:54:32 +0000

    updating views.py with sign up functionality

--- Budget_Manager/views.py
+++ Budget_Manager/views.py
@@ -3,6 +3,19 @@ from django.http import JsonResponse
 from .models import Income, Expense
 from .forms import IncomeForm, ExpenseForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required
 def dashboard(request):
