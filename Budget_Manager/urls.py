fec348b3fab7a6d4318cee13670e1b144be9cb1d
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-14 14:56:56 +0000

    updated urls to reflect views

--- Budget_Manager/urls.py
+++ Budget_Manager/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('delete_income/<int:income_id>/', views.delete_income, name='delete_income'),
     path('delete_expense/<int:expense_id>/', views.delete_expense, name='delete_expense'),
+    path('get_income/<int:income_id>/', views.get_income, name='get_income'),
+    path('get_expense/<int:expense_id>/', views.get_expense, name='get_expense'),
     path('signup/', views.SignUpView.as_view(), name='signup'),  # Sign-Up URL
     path('', views.dashboard, name='home'),
 ]
