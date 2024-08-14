a82b1d07c998b708c92ff86eb73049498c4ad939
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-14 06:20:48 +0000

    updating views to ensure add_income and add_expense are handled correctly and also removing and redundant add-income/expense.html references

--- Budget_Manager/views.py
+++ Budget_Manager/views.py
@@ -12,35 +12,8 @@ def dashboard(request):
     incomes = Income.objects.filter(user=request.user)
     expenses = Expense.objects.filter(user=request.user)
 
-    if request.method == 'POST':
-        if 'income_submit' in request.POST:
-            income_form = IncomeForm(request.POST)
-            if income_form.is_valid():
-                income = Income(
-                    user=request.user,
-                    source=income_form.cleaned_data['source'],
-                    planned_amount=income_form.cleaned_data['planned_amount'],
-                    received_amount=income_form.cleaned_data['received_amount'],
-                    date=income_form.cleaned_data['date']
-                )
-                income.save()
-                return redirect('dashboard')
-        elif 'expense_submit' in request.POST:
-            expense_form = ExpenseForm(request.POST)
-            if expense_form.is_valid():
-                expense = Expense(
-                    user=request.user,
-                    description=expense_form.cleaned_data['description'],
-                    planned_amount=expense_form.cleaned_data['planned_amount'],
-                    spent_amount=expense_form.cleaned_data['spent_amount'],
-                    category=expense_form.cleaned_data['category'],
-                    date=expense_form.cleaned_data['date']
-                )
-                expense.save()
-                return redirect('dashboard')
-    else:
-        income_form = IncomeForm()
-        expense_form = ExpenseForm()
+    income_form = IncomeForm()
+    expense_form = ExpenseForm()
 
     context = {
         'incomes': incomes,
@@ -60,9 +33,7 @@ def add_income(request):
             income.user = request.user
             income.save()
             return redirect('dashboard')
-    else:
-        form = IncomeForm()
-    return render(request, 'add_income.html', {'form': form})
+    return redirect('dashboard')
 
 @login_required
 def add_expense(request):
@@ -73,9 +44,7 @@ def add_expense(request):
             expense.user = request.user
             expense.save()
             return redirect('dashboard')
-    else:
-        form = ExpenseForm()
-    return render(request, 'add_expense.html', {'form': form})
+    return redirect('dashboard')
 
 @login_required
 def delete_income(request, income_id):
